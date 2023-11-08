5cc7c9e9a69d1c0e9c5371cf8525bb3bae6fa8cf
vrk <victoriakirst@gmail.com>
2023-11-08 17:34:39 -0500

    add all

---
+++ js/revert.js
@@ -0,0 +1,45 @@
+const explanation = document.getElementById("explanation");
+const realCameraBtn = document.getElementById("realcameraicon");
+const fakeCameraBtn = document.getElementById("fakecameraicon");
+const instructions = document.getElementById("iconinstructions");
+
+const onSelected = () => {
+  explanation.remove();
+  realCameraBtn.remove();
+  fakeCameraBtn.remove();
+  instructions.remove();
+}
+
+
+
+realCameraBtn.addEventListener('click', async () => {
+  onSelected();
+  try {
+    const videoElement = document.createElement('video');
+    const divElement = document.getElementById('backgroundcontainer');
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
+    videoElement.srcObject = stream;
+    videoElement.play();
+    divElement.appendChild(videoElement);
+    continueStory();
+  } catch (e) {
+    console.log(e);
+    showFake();
+  }
+});
+
+function showFake() {
+  const divElement = document.getElementById('backgroundcontainer');
+  divElement.classList.add("fake");
+  continueStory();
+}
+
+fakeCameraBtn.addEventListener('click', () => {
+  onSelected();
+  showFake();
+});
+
+function continueStory() {
+  document.getElementById('storycontinued').hidden = false;
+
+}
--- revert.md
+++ revert.md
@@ -1,8 +1,52 @@
 ---
 layout: main
 title: receipt printer rpg | pg 3
-# stylesheet: /css/notthis.css
-# script: /js/notthis.js
+stylesheet: /css/revert.css
+script: /js/revert.js
 ---
 
-crisis reverted
+<div class="bluegradient">
+  <div id="clouds">
+    you open your eyes
+  </div>
+
+
+  <div id="humanagain">
+    <p>You feel human again!!!!
+  </div>
+</div>
+
+<div id="phonegrab">
+  <div id="grabtoconfirm">
+    you grab your phone and open your camera app to confirm:
+  </div>
+
+  <div id="phonecase">
+    <div id="backgroundcontainer"></div>
+    <img src="/images/phone-background.png" id="phoneimg"/>
+    <img src="/images/fakecamera.png" id="fakecameraicon">
+    <img src="/images/realcamera.png" id="realcameraicon">
+    <div id="iconinstructions">
+    Choose an<br>app below:
+    </div>
+    <div id="explanation">
+      <p>if you click “Real Camera” I’m gonna request your actual camera<br>
+      <em>(nothing will be saved/uploaded)</em>
+      <p>if you click “Fake Camera” we’ll just fake it
+    </div>
+  </div>
+
+</div>
+
+<div id="storycontinued" hidden>
+
+<div id="gradientspacer">
+</div>
+
+<div id="hownow">
+  <p>
+    so how do you feel now?
+  </p>
+</div>
+
+</div>
