4990e5f7951f3a71935873319cfc3d55d9321b15
vrk <victoriakirst@gmail.com>
2023-11-16 13:49:04 -0500

    added

---
+++ apologize-pt-2.md
@@ -0,0 +1,59 @@
+---
+layout: main
+title: receipt printer rpg | pg 7
+stylesheet: /css/apologize-pt-2.css
+script: /js/apologize-pt-2.js
+---
+
+
+
+<div id="phoneout">
+
+<link rel="preload" as="image" href="/images/phone-bg-apology-transparent-cover.png">
+<link rel="preload" as="image" href="/images/phone-bg-apology.png">
+<link rel="preload" as="image" href="/images/fun-print-banner.png">
+<link rel="preload" as="image" href="/images/phone-with-fun-print-banner.png">
+
+
+<div id="receiptprinter" hidden>
+  <div id="receiptprintercontents">
+    <img src="/images/waiting.png">
+    <div class="replyreceipt">
+      <div class="hid-receipt">
+        it's OK friend :)
+      </div>
+    </div>
+    <div id="theend">the end</div>
+  </div>
+</div>
+
+<div id="phonegrab">
+  <div id="phonecase">
+    <div id="backgroundcontainer">
+      <div id="print-menu" hidden>
+        <div id="go-to-print-screen">
+          <img src="/images/fun-print-pencil.png">
+          <div>
+          Apologize
+          </div>
+        </div>
+      </div>
+      <img src="/images/fun-print-button.png" id="printappicon">
+      <div id="background-print-screen">
+        <div id="print-screen-contents">
+          <img src="/images/fun-print-printer.png" width="20%">
+          <textarea id="apologytextarea" placeholder="type apology here" autocomplete="off"></textarea>
+          <button id="printapologybutton" class="button-10" disabled> PRINT </button>
+        </div>
+      </div>
+    </div>
+    <div id="blackscreen"></div>
+    <div id="iconinstructions"></div>
+  </div>
+</div>
+
+<div id="bg">
+<div id="cloud2" class="cloud"></div>
+</div>
+
+</div>
---
+++ js/apologize-pt-2.js
@@ -0,0 +1,111 @@
+const printAppBtn = document.getElementById("printappicon");
+const instructions = document.getElementById("iconinstructions");
+const blackScreen = document.getElementById("blackscreen");
+const printMenu = document.getElementById("print-menu");
+const printScreenButton = document.getElementById("go-to-print-screen");
+const divElement = document.getElementById('backgroundcontainer');
+const printScreen = document.getElementById('background-print-screen');
+const printScreenContents = document.getElementById('print-screen-contents');
+const textarea = document.getElementById('apologytextarea');
+const printApologyButton = document.getElementById('printapologybutton');
+const apologySection = document.getElementById('apology');
+const phoneoutSection = document.getElementById('phoneout');
+const takeOutButton = document.getElementById('takeoutyourphone');
+const phonegrab = document.getElementById('phonegrab');
+const receiptPrinter = document.getElementById('receiptprinter');
+
+const wholePhone = document.getElementById('phonecase');
+const bg = document.getElementById('bg');
+
+const onSelected = () => {
+  printAppBtn.remove();
+  instructions.remove();
+  printMenu.hidden = true;
+  divElement.classList.remove("printer");
+}
+
+async function showPrintScreen() {
+  blackScreen.classList.add("shown");
+  printMenu.style.visibility = 'hidden';
+  printMenu.hidden = false;
+  await delay(350);
+  printMenu.style.visibility = 'visible';
+  blackScreen.hidden = true;
+}
+
+printAppBtn.addEventListener('click', () => {
+  onSelected();
+  showPrintScreen();
+});
+
+let buildingString = '';
+let message = "I am sorry.";
+let index = 0;
+textarea.addEventListener('beforeinput', (event) => {
+  event.preventDefault();
+  if (index === message.length) {
+    return;
+  }
+  buildingString += message[index];
+  textarea.innerHTML = buildingString;
+  index++;
+  textarea.setSelectionRange(buildingString.length, buildingString.length);
+  if (index === message.length) {
+    printApologyButton.disabled = false;
+  }
+});
+
+printScreenButton.addEventListener('click', async () => {
+  onSelected();
+  printScreen.hidden = false;
+  printScreen.classList.add('comein');
+  printScreenContents.classList.add('comein');
+  await delay(600);
+  textarea.focus();
+})
+
+printApologyButton.addEventListener('click', async () => {
+  if (printApologyButton.disabled) { return; }
+  printScreenContents.classList.add('printing');
+  for (let i = 0; i < 3; i++) {
+    printScreenContents.innerHTML = 'Printing.  ';
+    await delay(300);
+    printScreenContents.innerHTML = 'Printing.. ';
+    await delay(300);
+    printScreenContents.innerHTML = 'Printing...';
+    await delay(300);
+  }
+  await delay(300);
+
+  receiptPrinter.hidden = false;
+  receiptPrinter.style.opacity = 1;
+  wholePhone.style.opacity = 0;
+  wholePhone.style.transform = 'translateY(100px)';
+  await delay(1500);
+  wholePhone.style.visibility = 'hidden';
+  wholePhone.style.zIndex = -1000;
+  await delay(500);
+  document.querySelector('.hid-receipt').classList.add('show')
+  await delay(3500);
+  document.querySelector('#theend').style.opacity = 1;
+})
+
+takeOutButton.addEventListener('click', async () => {
+  apologySection.style.opacity = 0;
+  await delay(500);
+  apologySection.hidden = true;
+  phoneoutSection.hidden = false;
+  await delay(500);
+  wholePhone.style.opacity = 1;
+  wholePhone.style.transform = 'translateY(0)';
+  await delay(500);
+  bg.style.opacity = 1;
+})
+
+async function delay(timeInMs) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, timeInMs);
+  })
+} 
