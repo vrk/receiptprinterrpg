517fe505b0edfa633e7094be65cb49c78ea5a5ea
vrk <victoriakirst@gmail.com>
2023-11-21 14:00:36 -0500

    add lightning

--- evilprinter.md
+++ evilprinter.md
@@ -1,60 +1,14 @@
 ---
 layout: main
 title: receipt printer rpg | pg 11
-stylesheet: /css/nobleprinter.css
-script: /js/nobleprinter.js
+stylesheet: /css/evilprinter.css
+script: /js/evilprinter.js
 ---
 
-<div class="container" id="sakura">
-	<div class="sakura"><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-		<span></span><span></span><span></span><span></span><span></span>
-	</div>
-</div>
-
-<section id="selection-welcome">
-
-<p><img src="/images/choice-noble.png">
-
-Excellent, your life of service begins now.
-
-</section>
-
-<section id="selection-now-we-wait" class="hidden">
-
-<p>You wait for your first print job to come in...
-
-</section>
-
-<section id="orders" class="hidden" hidden>
-<div class="box orderbox" id="order1">
-  <h1>Order #1</h1>
-  <p id="response">Hello! I'd love a cool sticker please :)</p>
-
-  <div class="instructions" id="instructions">
-    <span class="emoji">️✏️</span>
-    Return to your paper zine!<br>
-    Turn to page 4 and follow the instructions.
+<div id="weather-beginning">
+  <canvas id="canvas"></canvas>
+  <div class="fog"></div>
+  <div id="verywell">
+    Very well.
   </div>
 </div>
-
-<div class="box narrow compbox" id="compbox1">
-  <form id="completion1">
-    <h2>Completion code:</h2>
-    <label>
-      <input type="number" min="0" maxlength="4" name="code" id="completioncode"/>
-    </label>
-    <input type="submit" value="next order" focused/>
-  </form>
-  <span id="message"></span>
-</div>
-
-</section>
-<div id="bg" class="hidden"></div>
--- js/evilprinter.js
+++ js/evilprinter.js
@@ -1,184 +1,157 @@
-const welcomeScreen = document.getElementById('selection-welcome');
-const waitScreen = document.getElementById('selection-now-we-wait');
-const orderScreen = document.getElementById('orders');
-const sakura = document.getElementById('sakura');
-const bg = document.getElementById('bg');
-const form1 = document.getElementById('completion1');
-const firstInput = form1.querySelector('input[type=number]');
-
-async function quickstart() {
-  welcomeScreen.classList.add("hidden");
-  welcomeScreen.hidden = true;
-  bg.classList.remove("hidden");
-  bg.classList.add("shown");
-  orderScreen.hidden = false;
-  orderScreen.classList.add("shown");
-  orderScreen.classList.remove("hidden");
-  await delay (5000);
-  sakura.classList.add("shown");
-  firstInput.focus();
-}
-
-async function start() {
-  await delay (4000);
-  welcomeScreen.classList.add("hidden");
-  await delay (1500);
-  welcomeScreen.hidden = true;
-  waitScreen.classList.remove("hidden");
-  waitScreen.classList.add("shown");
-  await delay (3000);
-  waitScreen.classList.add("hidden");
-  await delay (1500);
-  bg.classList.remove("hidden");
-  bg.classList.add("shown");
-  orderScreen.hidden = false;
-  await delay (500);
-  orderScreen.classList.add("shown");
-  orderScreen.classList.remove("hidden");
-  await delay (5000);
-  sakura.classList.add("shown");
-  firstInput.focus();
-}
-
-async function delay(timeInMs) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, timeInMs);
-  })
-} 
-
-start();
-// quickstart();
 
 
 
-const message = document.getElementById('message');
-const response = document.getElementById('response');
-const instructions = document.getElementById('instructions');
-form1.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  if (firstInput.value.trim() === '4321') {
-    firstInput.value = '';
-    response.innerHTML = "wowww it's so COOL!!"
-    message.innerHTML = "great work!!"
-    instructions.remove();
-    form1.remove();
-    await delay(2000);
-    questionTwo()
-  } else {
-    firstInput.value = '';
-    message.innerHTML = "incorrect code :("
-  }
-});
+/** adapted from https://codepen.io/mnald12/pen/RwvbvLq */
 
+const canvas = document.getElementById("canvas");
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+const ctx = canvas.getContext("2d");
 
-const orderBox2 = `
-<div class="box orderbox" id="order2">
-  <h1>Order #2</h1>
-  <p id="response2">can you please write me a poem?</p>
+const LEFT = "LEFT";
+const RIGHT = "RIGHT";
 
-  <div class="instructions" id="instructions2">
-    <span class="emoji">️✏️</span>
-    Return to your paper zine!<br>
-    Turn to page 5 and follow the instructions.
-  </div>
-</div>
-
-<div class="box narrow compbox" id="compbox2">
-  <form id="completion2">
-    <h2>Completion code:</h2>
-    <label>
-      <input type="number" min="0" maxlength="4" name="code" id="completioncode2"/>
-    </label>
-    <input type="submit" value="next order"/>
-  </form>
-  <span id="message2"></span>
-</div>
-`;
-
-const slideOutKeyframes = [
-  { 
-    transform: 'translateX(0)',
-    opacity: 1
-  },
-  {
-    transform: 'translateX(250px)',
-    opacity: 0
-  }
-];
-
-const slideOutTiming = {
-  duration: 500,
-  iterations: 1,
-  easing: 'ease-out',
-  fill: "forwards"
+const getDir = () => {
+  const dec = Math.floor(Math.random() * 30);
+  if (dec < 16) return LEFT;
+  else return RIGHT;
 };
 
-const fadeOutKeyframes = [
-  { opacity: 1},
-  { opacity: 0},
-];
-
-const fadeOutTiming = {
-  duration: 500,
-  iterations: 1,
-  easing: 'ease-out',
-  fill: "forwards"
-};
-
-async function questionTwo() {
-  const order1 = document.querySelector('#order1');
-  const compbox1 = document.querySelector('#compbox1');
-  const animation1 = order1.animate(fadeOutKeyframes, fadeOutTiming);
-  const animation2 = compbox1.animate(fadeOutKeyframes, fadeOutTiming);
-  await Promise.all([animation1.finished, animation2.finished]);
-  await delay(1000);
-  orderScreen.innerHTML = '';
-  orderScreen.innerHTML = orderBox2;
-  const form = orderScreen.querySelector('form');
-  const message = orderScreen.querySelector('#message2');
-  const response = orderScreen.querySelector('#response2');
-  const instructions = orderScreen.querySelector('#instructions2');
-  const input = form.querySelector('input[type=number]');
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    if (input.value.trim() === '3456') {
-      input.value = '';
-      response.innerHTML = "I love it :')"
-      message.innerHTML = "well done!!"
-      instructions.remove();
-      form.remove();
-      await delay(2000);
-      const thisOrderBox = orderScreen.querySelector('.orderbox');
-      const thisCompBox = orderScreen.querySelector('.compbox');
-      const animation3 = thisOrderBox.animate(fadeOutKeyframes, fadeOutTiming);
-      const animation4 = thisCompBox.animate(fadeOutKeyframes, fadeOutTiming);
-      await Promise.all([animation3.finished, animation4.finished]);
-      await delay(1000);
-      questionThree();
-    } else {
-      message.innerHTML = "incorrect code :("
+class Cloud {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = Math.floor(Math.random() * 30);
+    this.clr = "silver";
+    this.dir = getDir();
+    this.speed = Math.floor(Math.random() * 2) + 1;
+  }
+  moveLeft() {
+    this.x -= this.speed;
+  }
+  moveRight() {
+    this.x += this.speed;
+  }
+  update() {
+    if (this.x <= 0) {
+      this.dir = RIGHT;
+    } else if (this.x >= canvas.width) {
+      this.dir = LEFT;
     }
-  });
-  await delay(2000);
-  input.focus();
+
+    if (this.dir === LEFT) {
+      this.moveLeft();
+    } else {
+      this.moveRight();
+    }
+  }
+  drawRoot(x, y) {
+    let sx = x,
+      sy = y,
+      ex = sx + Math.floor(Math.random() * 50) - 15,
+      ey = sy + Math.floor(Math.random() * 30);
+    let i = 0,
+      limit = Math.floor(Math.random() * 20);
+    while (i < limit) {
+      ctx.beginPath();
+      ctx.strokeStyle = "silver";
+      ctx.lineWidth = 1;
+      ctx.moveTo(sx, sy);
+      ctx.lineTo(ex, ey);
+      ctx.stroke();
+      sx = ex;
+      sy = ey;
+      ex = sx + Math.floor(Math.random() * 50) - 15;
+      ey = sy + Math.floor(Math.random() * 30);
+      i++;
+    }
+  }
+  drawLightning() {
+    const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+    gradient.addColorStop(0, "rgba(25, 25, 25, 0.1)");
+    gradient.addColorStop(1, "rgba(0, 0, 0, 1)");
+    ctx.fillStyle = gradient
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    let sx = this.x,
+      sy = this.y,
+      ex = sx + Math.floor(Math.random() * 30) - 15,
+      ey = sy + Math.floor(Math.random() * 30);
+
+    let i = 0,
+      limit = Math.floor(Math.random() * 20) + 10;
+
+    while (i < limit) {
+      ctx.beginPath();
+      ctx.strokeStyle = "silver";
+      ctx.lineWidth = 3;
+      ctx.moveTo(sx, sy);
+      ctx.lineTo(ex, ey);
+      ctx.stroke();
+      sx = ex;
+      sy = ey;
+      ex = sx + Math.floor(Math.random() * 30) - 15;
+      ey = sy + Math.floor(Math.random() * 30);
+      let root = Math.floor(Math.random() * 1000);
+      if (root < 50) {
+        this.drawRoot(sx, sy);
+      }
+      i++;
+    }
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.clr;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+
+    const strike = Math.floor(Math.random() * 100000);
+
+    if (strike < 100) {
+      this.drawLightning();
+    }
+  }
 }
 
-async function questionThree() {
-  orderScreen.innerHTML = '';
-  orderScreen.innerHTML = orderBox3;
+const clouds = [];
+
+let i = 0;
+
+while (i < canvas.width) {
+  clouds.push(new Cloud(i, 0));
+  i += Math.floor(Math.random() * 10) + 1;
 }
 
-const orderBox3 = `
-<div class="box orderbox" id="order3">
-  <h1>Order #3</h1>
-  <p id="response3">hi receipt printer!! we have a message for you :D</p>
 
-  <div class="instructions" id="instructions2">
-    <span class="emoji">️✏️</span>
-    Return to your paper zine!<br>
-    Turn to page 7 and follow the instructions.
-  </div>
-</div>
-`;
+var fps = 5;
+var now;
+var then = Date.now();
+var interval = 1000/fps;
+var delta;
+  
+
+const animate = () => {
+       
+  now = Date.now();
+  delta = now - then;
+  if (delta > interval) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.shadowColor = "aliceblue";
+    ctx.shadowBlur = 10;
+
+    for (let c of clouds) {
+      c.draw();
+      c.update();
+    }
+    then = now - (delta % interval);
+  }
+
+  requestAnimationFrame(animate);
+};
+
+animate();
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
